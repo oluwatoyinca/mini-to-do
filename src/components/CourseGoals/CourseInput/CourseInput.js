a0a4d8836f73942d54ee957695840294274a18ba
Oluwatoyin Amushan <alexandradonalds@gmail.com>
2022-03-21 01:48:45 +0100

    signified that filed is required with *

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -61,7 +61,7 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
-        <label>Course Goal</label>
+        <label>Course Goal *</label>
         <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
